99110604789a6b22e4ed595baa08399118b47f56
yamayuu <yamayuu.ics@gmail.com>
2018-03-14 20:46:50 +0900

    Docker file
    Ubuntu 16.04
    ros kinetic

---
+++ ubuntu_16.04_ROS_kinetic/ros_run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "start ros on docker"
+echo ""
+
+echo "for GUI xhost local:"
+xhost local:
+echo ""
+
+
+echo "run ubuntu_ros_k Docker image"
+echo "mount /home/yamayuu/work as /home/yamayuu/work"
+echo ""
+#docker run -ti  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix ubuntu_ros_k /bin/bash
+
+docker run -ti  -e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix/:/tmp/.X11-unix \
+	-v /home/yamayuu/work/:/home/yamayuu/work \
+	ubuntu_ros_k /bin/bash
