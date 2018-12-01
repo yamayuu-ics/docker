28edcc96fd104eb85f2452b2692fb4a5e1c37efb
yamayuu <yamayuu.ics@gmail.com>
2018-12-01 18:12:29 +0900

    change comment

--- mxnet/R/run.sh
+++ mxnet/R/run.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-echo "start ros on docker"
+echo "start R with mxnet on docker"
 echo ""
 
 echo "for GUI xhost local:"
@@ -8,12 +8,14 @@ xhost local:
 echo ""
 
 
-echo "run ubuntu_ros_k Docker image"
+echo "run Docker image"
 echo "mount /home/yamayuu/work as /home/yamayuu/work"
+echo "mount /data as /data"
 echo ""
 #docker run -ti  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix ubuntu_ros_k /bin/bash
 
 nvidia-docker run -ti --rm  -e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix \
 	-v /home/yamayuu/work/:/home/yamayuu/work \
+	-v /data:/data \
 	mxnet_r_cuda8_dnn5:$1
