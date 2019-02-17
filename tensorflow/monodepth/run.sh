26e636315ecbd6407b980748947f9d1d65520dff
yamayuu <yamayuu.ics@gmail.com>
2019-02-17 17:10:47 +0900

    change python version and some library

--- tensorflow/monodepth/run.sh
+++ tensorflow/monodepth/run.sh
@@ -1,10 +1,6 @@
 #/bin/sh
 
-echo "start R with mxnet on docker"
-echo ""
-
-echo "for GUI xhost local:"
-xhost local:
+echo "tensorflow on docker"
 echo ""
 
 
@@ -14,8 +10,8 @@ echo "mount /data as /data"
 echo ""
 #docker run -ti  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix ubuntu_ros_k /bin/bash
 
-nvidia-docker run -ti --rm  -e DISPLAY=$DISPLAY \
-	-v /tmp/.X11-unix/:/tmp/.X11-unix \
+nvidia-docker run -ti --rm --net host -e DISPLAY=$DISPLAY \
+	-v $HOME/.Xauthority:$HOME/.Xauthority:rw \
 	-v /home/yamayuu/work/:/home/yamayuu/work \
 	-v /data:/data \
-	mxnet_r_cuda8_dnn5:$1
+	tf_1.0_monodepth
