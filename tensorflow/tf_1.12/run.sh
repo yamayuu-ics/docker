b77cca870ea8722a99bd9f2a81fd2ed9af284c92
yamayuu <yamayuu.ics@gmail.com>
2019-03-06 22:45:10 +0900

    tensorflow 1.12

---
+++ tensorflow/tf_1.12/run.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+cat $0
+
+nvidia-docker run -ti --rm --net host -e DISPLAY=$DISPLAY \
+	-v $HOME/.Xauthority:$HOME/.Xauthority:rw \
+	-v /home/yamayuu/work/:/home/yamayuu/work \
+	-v /data:/data \
+	tf_test /bin/bash
